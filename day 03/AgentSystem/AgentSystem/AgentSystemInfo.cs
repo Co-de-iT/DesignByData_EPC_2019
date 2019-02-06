524592bd121b4f2f95e3154459c5f5b13592bbcf
Alessio Erioli <alessio.erioli@gmail.com>
2019-02-06 10:17:12 +0100

    Agent Systen upload

---
+++ day 03/AgentSystem/AgentSystem/AgentSystemInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace AgentSystem
+{
+    public class AgentSystemInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "AgentSystem";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("67b5f9f5-ef7e-43df-a206-3127a582286c");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
